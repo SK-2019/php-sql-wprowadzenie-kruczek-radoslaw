3bd391ffb7930b55c38a984e956837f6e4aeadd0
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-10-30 13:04:48 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 echo("<h1>Radosław Kruczek</h1>");
+$conn = new mysqli('sql7.freemysqlhosting.net', 'sql7373527', 'uQnUsHq6FQ', 'sql7373527');
+$sql = 'SELECT * FROM tabela';
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>");
+        echo("</tr>");
+    }
+echo("</table>");
 ?>
