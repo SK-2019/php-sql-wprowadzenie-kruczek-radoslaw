adee3a882abb3f06d444348b8c0ba3ae52057e93
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-10-30 14:23:43 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -17,3 +17,79 @@ echo("<th>zarobki</th>");
     }
 echo("</table>");
 ?>
+<?php
+//echo("<h1>Radosław Kruczek</h1>");
+$conn = new mysqli('sql7.freemysqlhosting.net', 'sql7373527', 'uQnUsHq6FQ', 'sql7373527');
+$sql = 'SELECT * FROM where imie like "%a"';
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>");
+        echo("</tr>");
+    }
+echo("</table>");
+?>
+<?php
+//echo("<h1>Radosław Kruczek</h1>");
+$conn = new mysqli('sql7.freemysqlhosting.net', 'sql7373527', 'uQnUsHq6FQ', 'sql7373527');
+$sql = 'SELECT * FROM tabela where dzial in(1, 2)';
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>");
+        echo("</tr>");
+    }
+echo("</table>");
+?>
+<?php
+//echo("<h1>Radosław Kruczek</h1>");
+$conn = new mysqli('sql7.freemysqlhosting.net', 'sql7373527', 'uQnUsHq6FQ', 'sql7373527');
+$sql = 'SELECT * FROM tabela where imie not like "%a" and dzial in(1, 3)';
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>");
+        echo("</tr>");
+    }
+echo("</table>");
+?>
+<?php
+//echo("<h1>Radosław Kruczek</h1>");
+$conn = new mysqli('sql7.freemysqlhosting.net', 'sql7373527', 'uQnUsHq6FQ', 'sql7373527');
+$sql = 'SELECT * FROM tabela where zarobki >30';
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
+echo("<th>dzial</th>");
+echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>");
+        echo("</tr>");
+    }
+echo("</table>");
+?>
