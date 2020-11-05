9ad05c58c2fddef24930a93a197b827630fb9203
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-05 11:03:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <div class="nav">
     <a href="orgPracownicy.php">Org i Prac </a>
     <a href="agregat.php">Funkcje Agregujące </a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github </a>
 </div>
 <?php
     echo("<h1>Radosław Kruczek</h1>");
