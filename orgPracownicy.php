d6e5849e03268c59ce95111b0de13b5db294df82
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-06 14:57:03 +0100

    Add files via upload

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -8,112 +8,12 @@
     <a href="agregat.php">Funkcje Agregujące </a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github </a>
 </div>
-<?php
-    echo("<h1>Radosław Kruczek</h1>");
-    require_once("connect.php");
-    $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org';
-    echo("<h2>Zadanie 1</h2>");
-    echo("<h3>Wyświetl wszystkich pracowników</h3>");
-    echo("<li>".$sql);
-    $result = $conn->query($sql);
-    echo("<table border=1>");
-    echo("<th>id</th>");
-    echo("<th>imie</th>");
-    echo("<th>nazwa_dzial</th>");
-    echo("<th>zarobki</th>");
 
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
-?>
-<?php
-require_once("connect.php");
-$sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie like "%a"';
-echo("<h2>Zadanie 2</h2>");
-echo("<h3>Wyświetl wszystkie kobiety</h3>");
-echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=1>");
-echo("<th>id</th>");
-echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");
-
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>"); 
-        echo("</tr>");
-    }
-echo("</table>");
-?>
-<?php
-require_once("connect.php");
-$sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial in(1, 2)';
-echo("<h2>Zadanie 3</h2>");
-echo("<h3>Wyświetl wszystkich pracowników z działu serwis i handel</h3>");
-echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=1>");
-echo("<th>id</th>");
-echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");
-
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
-?>
-<?php
-require_once("connect.php");
-$sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie not like "%a" and dzial in(1, 3)';
-echo("<h2>Zadanie 4</h2>");
-echo("<h3>Wyświetl wszystkich męszczyzn z działu serwis i marketing</h3>");
-echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=1>");
-echo("<th>id</th>");
-echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");
-
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
-?>
-<?php
-require_once("connect.php");
-$sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and zarobki >30';
-echo("<h2>Zadanie 5</h2>");
-echo("<h3>Wyświetl wszystkich pracowników zarabiających więcej niż 30</h3>");
-echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=1>");
-echo("<th>id</th>");
-echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");
-
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
-?>
 
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org';
-echo("<h2>Zadanie 6</h2>");
+echo("<h2>Zadanie 1</h2>");
 echo("<h3>Pracownicy z nazwą działów</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -134,7 +34,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial in(1, 4)';
-echo("<h2>Zadanie 7</h2>");
+echo("<h2>Zadanie 2</h2>");
 echo("<h3>Pracownicy z działu 1 i 4</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -154,7 +54,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie like "%a"';
-echo("<h2>Zadanie 8</h2>");
+echo("<h2>Zadanie 3</h2>");
 echo("<h3>Lista Kobiet z nazwami działów</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -174,7 +74,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie not like "%a"';
-echo("<h2>Zadanie 9</h2>");
+echo("<h2>Zadanie 4</h2>");
 echo("<h3>Lista Mężczyzn z nazwami działów</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -194,7 +94,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org order by imie desc';
-echo("<h2>Zadanie 10</h2>");
+echo("<h2>Zadanie 5</h2>");
 echo("<h3>pracownicy posortowani malejąco wg imienia</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -214,7 +114,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial = 3 order by imie asc';
-echo("<h2>Zadanie 11</h2>");
+echo("<h2>Zadanie 6</h2>");
 echo("<h3>pracownicy z działu 3 posortowani rosnąco po imieniu</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -234,7 +134,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie like "%a" order by imie asc';
-echo("<h2>Zadanie 12</h2>");
+echo("<h2>Zadanie 7</h2>");
 echo("<h3>kobiety posortowane po imieniu rosnąco</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -254,7 +154,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial in(1, 3)  and imie like "%a" order by zarobki asc';
-echo("<h2>Zadanie 13</h2>");
+echo("<h2>Zadanie 8</h2>");
 echo("<h3>kobiety z działu 1 i 3 posortowane rosnąco po zarobkach</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -274,7 +174,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie not like "%a" order by id_org asc';
-echo("<h2>Zadanie 14</h2>");
+echo("<h2>Zadanie 9</h2>");
 echo("<h3>Mężczyźni posortowani rosnąco po nazwie działu</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
@@ -294,7 +194,7 @@ echo("</table>");
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and imie not like "%a" order by zarobki asc';
-echo("<h2>Zadanie 15</h2>");
+echo("<h2>Zadanie 10</h2>");
 echo("<h3>Mężczyźni posortowani rosnąco po wysokości zarobków</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
