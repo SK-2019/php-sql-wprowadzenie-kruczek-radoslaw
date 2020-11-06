80ac482e4115047c8f94f49838c649139e925f02
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-06 10:54:34 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <a href="orgPracownicy.php">Org i Prac </a>
     <a href="agregat.php">Funkcje Agregujące </a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github </a>
+    <a href="pracownicy.php">pracownicy</a>
 </div>
 <?php
     echo("<h1>Radosław Kruczek</h1>");
---
+++ pracownicy.php
@@ -0,0 +1,68 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+</html>
+<div class="nav">
+    <a href="index.php">strona główna </a>
+    <a href="agregat.php">Funkcje Agregujące </a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github </a>
+    <a href="orgPracownicy.php">Org i Prac </a>
+</div>
+<?php
+    echo("<h1>Radosław Kruczek</h1>");
+    require_once("connect.php");
+    $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial = 2';
+    echo("<h2>Zadanie 1</h2>");
+    echo("<h3>Pracownicy tylko z działu 2</h3>");
+    echo("<li>".$sql);
+    $result = $conn->query($sql);
+    echo("<table border=1>");
+    echo("<th>id</th>");
+    echo("<th>imie</th>");
+    echo("<th>nazwa_dzial</th>");
+    echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("</tr>");
+    }
+    echo("</table>");
+
+    $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and dzial in(2, 3)';
+    echo("<h2>Zadanie 2</h2>");
+    echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
+    echo("<li>".$sql);
+    $result = $conn->query($sql);
+    echo("<table border=1>");
+    echo("<th>id</th>");
+    echo("<th>imie</th>");
+    echo("<th>nazwa_dzial</th>");
+    echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("</tr>");
+    }
+    echo("</table>");
+
+    $sql = 'SELECT * FROM tabela, organizacja where dzial = id_org and zarobki < 30';
+    echo("<h2>Zadanie 3</h2>");
+    echo("<h3>Pracownicy tylko z działu 2</h3>");
+    echo("<li>".$sql);
+    $result = $conn->query($sql);
+    echo("<table border=1>");
+    echo("<th>id</th>");
+    echo("<th>imie</th>");
+    echo("<th>nazwa_dzial</th>");
+    echo("<th>zarobki</th>");
+
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['id']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("</tr>");
+    }
+    echo("</table>");
+    ?>
