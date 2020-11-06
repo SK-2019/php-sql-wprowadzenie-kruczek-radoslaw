c9bb814fa8b9f327d0ff34132994d1ec37cd4781
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-06 15:18:03 +0100

    Add files via upload

--- agregat.php
+++ agregat.php
@@ -137,3 +137,19 @@ echo("<th>ilosc_kobiet</th>");
     }
 echo("</table>");
 ?>
+<?php
+require_once("connect.php");
+$sql = 'SELECT sum(imie) as suma_zarobki FROM tabela, organizacja where dzial = id_org';
+echo("<h2>Zadanie 9</h2>");
+echo("<h3>suma zarobków w poszczególnych działach</h3>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>nazwa_dzial</th>");
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['nazwa_dzial']."</td>"); 
+        echo("</tr>");
+    }
+echo("</table>");
+?>
