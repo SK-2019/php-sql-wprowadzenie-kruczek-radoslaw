4903cceed559ecadd7c71b3fb1a059f298a9b9aa
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-06 15:24:28 +0100

    Add files via upload

--- agregat.php
+++ agregat.php
@@ -145,10 +145,14 @@ echo("<h3>suma zarobków w poszczególnych działach</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
 echo("<table border=1>");
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>imie</th>");
 echo("<th>nazwa_dzial</th>");
+echo("<th>zarobki</th>");
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-        echo("<td>".$wiersz['nazwa_dzial']."</td>"); 
+        echo("<td>".$wiersz['nazwa_dzial']."</td>""<td>".$wiersz['zarobki']."</td>"); 
         echo("</tr>");
     }
 echo("</table>");
