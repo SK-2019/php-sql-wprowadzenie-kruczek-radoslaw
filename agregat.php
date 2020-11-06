57ba0c266f690e5a4341a914575017dfc72c4c7c
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-06 15:02:34 +0100

    Add files via upload

--- agregat.php
+++ agregat.php
@@ -8,62 +8,67 @@
     <a href="orgPracownicy.php">Org i Prac </a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github </a>
 </div>
+
 <?php
 require_once("connect.php");
-$sql = 'SELECT max(zarobki) as maksymalne_zarobki FROM tabela, organizacja where dzial = id_org';
-echo("<h2>Zadanie 1</h2>");
-echo("<h3>Wyświetl maksymalne zarobki</h3>");
+$sql = 'SELECT sum(zarobki) as suma_zarobki FROM tabela, organizacja where dzial = id_org';
+echo("<h2>Zadanie 4</h2>");
+echo("<h3>suma zrobków wszystkich pracowników</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
 echo("<table border=1>");
-echo("<th>maksymalne_zarobki</th>");
-/*echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");*/
-
+echo("<th>suma_zarobki</th>");
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-        echo("<td>".$wiersz['maksymalne_zarobki']."</td>"); //<td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("<td>".$wiersz['suma_zarobki']."</td>"); 
         echo("</tr>");
     }
 echo("</table>");
 ?>
 <?php
 require_once("connect.php");
-$sql = 'SELECT avg(zarobki) as srednia_zarobkow FROM tabela, organizacja where dzial = id_org and dzial in(1, 3)';
-echo("<h2>Zadanie 2</h2>");
-echo("<h3>Wyświetl średnią zarobków z działów serwis i marketing</h3>");
+$sql = 'SELECT sum(zarobki) as suma_zarobki_kobiet FROM tabela, organizacja where dzial = id_org and imie like "%a"';
+echo("<h2>Zadanie 5</h2>");
+echo("<h3>suma zarobki kobiet</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
 echo("<table border=1>");
-echo("<th>srednia_zarobkow</th>");
-/*echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");*/
-
+echo("<th>suma_zarobki_kobiet</th>");
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-        echo("<td>".$wiersz['srednia_zarobkow']."</td>"); //<td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("<td>".$wiersz['suma_zarobki_kobiet']."</td>"); 
         echo("</tr>");
     }
 echo("</table>");
 ?>
 <?php
 require_once("connect.php");
-$sql = 'SELECT min(zarobki) as minimalne_zarobki FROM tabela, organizacja where dzial = id_org';
-echo("<h2>Zadanie 3</h2>");
-echo("<h3>Wyświetl minmalne zarobki</h3>");
+$sql = 'SELECT sum(zarobki) as suma_zarobki_mezczyzn FROM tabela, organizacja where dzial = id_org and imie not like "%a" and dzial in(2, 3)';
+echo("<h2>Zadanie 6</h2>");
+echo("<h3>suma zarobki mężczyzn w dziale 2 i 3</h3>");
 echo("<li>".$sql);
 $result = $conn->query($sql);
 echo("<table border=1>");
-echo("<th>minimalne_zarobki</th>");
-/*echo("<th>imie</th>");
-echo("<th>nazwa_dzial</th>");
-echo("<th>zarobki</th>");*/
-
+echo("<th>suma_zarobki_mezczyzn</th>");
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-        echo("<td>".$wiersz['minimalne_zarobki']."</td>"); //<td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td>");
+        echo("<td>".$wiersz['suma_zarobki_mezczyzn']."</td>"); 
+        echo("</tr>");
+    }
+echo("</table>");
+?>
+<?php
+require_once("connect.php");
+$sql = 'SELECT avg(zarobki) as srednia_zarobki_mezczyzn FROM tabela, organizacja where dzial = id_org and imie not like "%a" and dzial in(2, 3)';
+echo("<h2>Zadanie 7</h2>");
+echo("<h3>średnia zarobków mężczyzn</h3>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>srednia_zarobki_mezczyzn</th>");
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['srednia_zarobki_mezczyzn']."</td>"); 
         echo("</tr>");
     }
 echo("</table>");
