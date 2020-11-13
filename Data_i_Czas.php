7ea8c858e392b7e1345f727f136f67403eff7d8d
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-13 14:07:56 +0100

    Add files via upload

--- Data_i_Czas.php
+++ Data_i_Czas.php
@@ -250,7 +250,7 @@ echo("<th>zarobki</th>");
 echo("<th>data urodzenia</th>");
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-        echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['DATE_FORMAT(data_urodzenia,"%W-%m-%Y")']."</td>");
+        echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['DATE_FORMAT(data_urodzenia,"%W")']."</td>");
         echo("</tr>");
     }
 echo("</table>");
