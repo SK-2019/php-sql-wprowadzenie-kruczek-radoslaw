752ba4d66e9b7df5f24dccfe1bee4bf1c642c707
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-17 09:55:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     </head>
     <body>
     <form action="strona.php" method="POST">
-        <input type="text" name="name"> 
+        <input type="text" name="firstname"></br>
+        <input type="text" name="lastname"></br>
+        <input type="text" name="password"></br>
+        <input type="text" name="title"></br>
         <input type="submit" name="wyślij w świat">
     </form>
 </body>
