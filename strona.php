ac919a7a859bcf9f80307140393863640778201b
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-17 10:08:53 +0100

    Add files via upload

--- strona.php
+++ strona.php
@@ -1,8 +1,8 @@
 <?php
 
 echo("<h1>strona.php</h1>");
-echo("<br>imie:".$_POST['firstname']);
-echo("<br>nazwisko:".$_POST['lastname']);
-echo("<br>hasło:".$_POST['password']);
-echo("<br>tytuł:".$_POST['title']);
+echo("<br>imie: ".$_POST['firstname']);
+echo("<br>nazwisko: ".$_POST['lastname']);
+echo("<br>hasło: ".$_POST['password']);
+echo("<br>tytuł: ".$_POST['title']);
 ?>
