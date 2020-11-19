30b14b9a202ba956f8117345fbf22ea546661553
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-19 21:43:41 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 require_once("connect.php");
-$sql = "INSERT INTO Pracownicy (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
