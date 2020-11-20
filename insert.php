0074be44c8c4b5e3021a467ef472f607bdd4a107
Tomjand <domagala.tomasz@gmail.com>
2020-11-20 11:14:25 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,25 +1,25 @@
 <?php
 echo("jestes w insert.php <br>");
-echo "<li>". $_POST['name'];
-echo "<li>". $_POST['dzial'];
-echo "<li>". $_POST['zarobki'];
-echo "<li>". $_POST['data_urodzenia'];
+// echo "<li>". $_POST['name'];
+// echo "<li>". $_POST['dzial'];
+// echo "<li>". $_POST['zarobki'];
+// echo "<li>". $_POST['data_urodzenia'];
 
 
-require_once("connect.php");
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzenia)
-	      VALUES (
-					null, 
-					$_POST['name'], 
-					$_POST['dzial'], 
-					$_POST['zarobki'],
-					$_POST['data_urodzenia'] )
-				";
+// require_once("connect.php");
+// $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzenia)
+// 	      VALUES (
+// 					null, 
+// 					$_POST['name'], 
+// 					$_POST['dzial'], 
+// 					$_POST['zarobki'],
+// 					$_POST['data_urodzenia'] )
+// 				";
 
 
-echo "<li>". $sql;
+// echo "<li>". $sql;
 
-$conn->query($sql);
+// $conn->query($sql);
 
-$conn->close();
+// $conn->close();
 ?>
