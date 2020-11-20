754fd3eadc7ae89e40530b071ebcb2dd47207d07
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-20 10:55:15 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,8 +1,29 @@
 <?php
-require_once("connect.php");
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+echo("jestes w insert.php");
+echo "<li>". $_POST['name'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
-$conn->query($sql);
+
+require_once("connect.php");
+$sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
+	      VALUES (
+					null, 
+					$_POST['name'], 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					$_POST['data_urodzenia']
+				";
+
+
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
