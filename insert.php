03089e22c12b1e1e16fb0cd10deca8b4fa582014
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-20 11:22:22 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php
-echo("jestes w insert.php <br>");
+echo("jestes w insert.php </br>");
 echo "<li>". $_POST['imie'];
 echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
@@ -7,19 +7,12 @@ echo "<li>". $_POST['data_urodzenia'];
 
 
 require_once("connect.php");
-//$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzenia)
-//	      VALUES (
-// 					null, 
-//					$_POST['imie'], 
-//					$_POST['dzial'], 
-//					$_POST['zarobki'],
-//					$_POST['data_urodzenia'] )
-//				";
-//
-//
-//  echo "<li>". $sql;
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) VALUES (null, $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia'])";
 
-// $conn->query($sql);
 
-// $conn->close();
+echo "<li>". $sql;
+
+$conn->query($sql);
+
+$conn->close();
 ?>
