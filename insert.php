566c8c13abc21808dbf8a68fda902286c65646d4
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-20 11:18:35 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,20 +6,20 @@ echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
 
 
-// require_once("connect.php");
-// $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzenia)
-// 	      VALUES (
-// 					null, 
-// 					$_POST['name'], 
-// 					$_POST['dzial'], 
-// 					$_POST['zarobki'],
-// 					$_POST['data_urodzenia'] )
-// 				";
+require_once("connect.php");
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzenia)
+	      VALUES (
+ 					null, 
+					$_POST['imie'], 
+					$_POST['dzial'], 
+ 					$_POST['zarobki'],
+					$_POST['data_urodzenia'] )
+				";
 
 
-// echo "<li>". $sql;
+ echo "<li>". $sql;
 
-// $conn->query($sql);
+ $conn->query($sql);
 
-// $conn->close();
+ $conn->close();
 ?>
