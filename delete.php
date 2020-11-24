cce3e09e64709547d9b759ed28fa726848bd6143
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-24 09:51:21 +0100

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+echo("jestes w delete.php <br>");
+echo $_POST['id'];
+
+
+
+require_once("connect.php");
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy= $_POST['id'];";
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
