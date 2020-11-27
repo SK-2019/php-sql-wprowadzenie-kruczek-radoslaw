b2a669c7e314e05fb1a14bd2cd06c55352d1ff15
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-27 11:06:44 +0100

    Update daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -19,11 +19,7 @@
         <input type="date" name="data_urodzenia" placeholder="data urodzenia"></br>
 		<input type="submit" value="dodaj pracownika"></br>
 	</form>
-	<h3>usuwanie pracownika</h3>
-	<form action="delete.php" method="POST">
-		<input type="number" name="id" placeholder="id"></br>
-		<input type="submit" value="Zapisz w zmiennej $_POST['id']">
-	</form>
+	
 </body>
 </html>
 
