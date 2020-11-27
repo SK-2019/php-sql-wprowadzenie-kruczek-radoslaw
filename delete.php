b6bccfb91bc9ced113081ac0ba2218f9a770e284
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-27 14:44:02 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -31,6 +31,7 @@ echo $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo ("<h2>Record deleted successfully</h2>");
+  header('Location: daneDoBazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
