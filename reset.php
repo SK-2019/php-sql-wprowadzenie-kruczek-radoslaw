d97035773163e44cff27b302f6af3116746ca13e
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-11-27 15:46:03 +0100

    Add files via upload

--- reset.php
+++ reset.php
@@ -1,18 +1,22 @@
 <?php
 
 require_once("connect.php");
+
+system('mysql --user=radek --password=Radek2003! Radek_009< D:\Pobrane\bazaPracownicyOrganizacja.sql');
+
+
 //definiujemy zapytanie $sql
-$sql ="source '".$_POST['com']."' ";
+//$sql ="source '".$_POST['com']."' ";
 
 //wyświetlamy zapytanie $sql
-echo $sql;
+//echo $sql;
 
-if ($conn->query($sql) === TRUE) {
+//if ($conn->query($sql) === TRUE) {
   echo ("<h2>Record deleted successfully</h2>");
-  header('Location: daneDoBazy.php');
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  
+} //else {
+  //echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
+header('Location: daneDoBazy.php');
 $conn->close();
 ?>
