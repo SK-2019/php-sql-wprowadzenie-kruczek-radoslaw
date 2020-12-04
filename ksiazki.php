24e76f4af95191fe70b751f9729a42a8dd42c13f
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-12-04 11:10:46 +0100

    Add files via upload

--- ksiazki.php
+++ ksiazki.php
@@ -2,7 +2,7 @@
     <head>
         <link rel="stylesheet" href="style.css">
     </head>
-</html>
+
 <div class="nav">
     <a href="pracownicy.php">pracownicy</a>
     <a href="agregat.php">Funkcje Agregujące</a>
@@ -11,23 +11,27 @@
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a>
 </div>
 
-
 <?php
 require_once("connect.php");
-$sql = 'SELECT * FROM biblAutor';
-echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>id</th>");
-echo("<th>autor</th>");
 
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id']."</td><td>".$wiersz['autor']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+$sql = ("SELECT * from biblTytul");
+echo("<h2>".$sql."</h2>");
+
+$result=$conn->query($sql);
+        echo("<select name='tytul' id='tytul'>");
+
+        while($row=$result->fetch_assoc()) {
+               
+                    echo("<option value=".$wiersz['tytul'].">".$wiersz["tytul"]."</option>");}
+                    echo("<input type='Submit' value='Sumbit'><br>");
+               
+            
+        echo("</select>");
+
+
+
 ?>
+</body>
 <?php
 require_once("connect.php");
 $sql = 'SELECT * FROM biblTytul';
