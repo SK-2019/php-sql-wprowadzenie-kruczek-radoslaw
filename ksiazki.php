6c2a4c425ef15417cd9900370404c47b40a0700b
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-12-04 15:10:18 +0100

    Add files via upload

--- ksiazki.php
+++ ksiazki.php
@@ -25,7 +25,7 @@ $result=$conn->query($sql);
 
         while($wiersz=$result->fetch_assoc()) {
                
-                    echo("<option value=".$wiersz['id'].">".$wiersz["autor"]."<br></option>");}
+                    echo("<option value=".$wiersz['id'].">".$wiersz["autor"]."</option>");}
                     //echo("<input type='Submit' value='Submit'><br>");
                
             
@@ -46,7 +46,7 @@ $result=$conn->query($sql);
 
         while($wiersz=$result->fetch_assoc()) {
                
-                    echo("<option value=".$wiersz['id'].">".$wiersz["tytul"]."<br></option>");}
+                    echo("<option value=".$wiersz['id'].">".$wiersz["tytul"]."</option>");}
                     echo("<input type='Submit' value='Submit'><br>");
                
             
