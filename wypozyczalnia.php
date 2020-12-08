fd07c67fe41fa78558c4ff4c7a150d62edcb770d
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-12-08 09:15:59 +0100

    Add files via upload

--- wypozyczalnia.php
+++ wypozyczalnia.php
@@ -12,9 +12,13 @@ echo("<li>tytuł: ".$_POST['tytul']);
 
 require_once("connect.php");
 
-$sql = ("INSERT INTO biblAutor_biblTytul(id, biblAutor_id, biblTytul_id) VALUES (null,'".$_POST['autor']."','".$_POST['tytul']."')";
+$sql = ("INSERT INTO biblAutor_biblTytul(id, biblAutor_id, biblTytul_id) VALUES(null,".$_POST['autor'].",".$_POST['tytul']."");
 
 
 $result=$conn->query($sql);
+
+$conn->close();
+
+header('Location: ksiazki.php');
 
 ?>
