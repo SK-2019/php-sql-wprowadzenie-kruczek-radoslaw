60322a7258fb1e774afde48c6cc31d76edd8a914
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-12-11 11:15:18 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Static Template</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        <h1 class="title">Radosław Kruczek</h1>
+      </div>
+      <div class="item colorBlue">
+        <a href="orgPracownicy.php">Org i Prac</a>
+        <a href="agregat.php">Funkcje Agregujące</a>
+        <a href="pracownicy.php">pracownicy</a>
+        <a href="Data_i_Czas.php">Data i czas</a>
+        <a
+          href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw"
+          >Github</a
+        >
+        <a href="daneDoBazy.php">Dane do Bazy</a>
+        <a href="ksiazki.php">Książki</a>
+      </div>
+      <div class="item colorGreen"></div>
+    </div>
+  </body>
 </html>
-<div class="nav">
-    <a href="orgPracownicy.php">Org i Prac</a>
-    <a href="agregat.php">Funkcje Agregujące</a>
-    <a href="pracownicy.php">pracownicy</a>
-    <a href="Data_i_Czas.php">Data i czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a>
-    <a href="daneDoBazy.php">Dane do Bazy</a>
-    <a href="ksiazki.php">Książki</a>
-    
-</div>
-<?php
-    echo("<h1>Radosław Kruczek</h1>");
-    
-?>
 
 
