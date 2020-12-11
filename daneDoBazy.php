51f3571ce5b0a78d1c60fd6a1a1a8c79f3307385
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2020-12-11 14:39:37 +0100

    Add files via upload

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -44,7 +44,7 @@ echo("<th>data_urodzenia</th>");
 
     while($wiersz=$result->fetch_assoc()){
         echo("<tr>");
-          echo("<td>".$wiersz["id_pracownicy"]."</td><td>".$wiersz["imie"]."</td><td>".$wiersz["dzial"]."</td><td>".$wiersz["zarobki"]."</td><td>".$wiersz["data_urodzenia"]."</td>
+          echo("<td>".$wiersz["id_pracownicy"]."</td><td>".$wiersz["imie"]."</td><td>".$wiersz["nazwa_dzial"]."</td><td>".$wiersz["zarobki"]."</td><td>".$wiersz["data_urodzenia"]."</td>
 		
 		<td>
 		
