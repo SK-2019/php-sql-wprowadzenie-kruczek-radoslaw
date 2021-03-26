582a6e574286fad4f892ef14d5d0d44857c11dbe
Radek <r.kruczek03@gmail.com>
2021-03-26 08:14:08 +0100

    flexbox

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
         <li class="nav_item"><a href="Pracownicy/Data_i_Czas.php">Data i czas</a></li>
         <li class="nav_item"><a href="Dane-do-bazy/daneDoBazy.php">Dane do Bazy</a></li>
         <li class="nav_item"><a href="Biblioteka/ksiazki.php">Książki</a></li>
+        <li class="nav_item"><a href="cwiczenia/flexbox.html">Flexbox</a></li>
         <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
         </ul>
       </div>
