cb9bf4f4deef24071d8db55aa0e59605d5dc19cc
Radek <r.kruczek03@gmail.com>
2021-04-20 17:59:22 +0200

    menu

--- strona.php
+++ strona.php
@@ -3,16 +3,16 @@
 $navbar = <<<NAV
 
 <ul class="nav">
-        <li class="nav_item"><a href="/Pracownicy/orgPracownicy.php">Organizacja i Pracownicy</a></li>
-        <li class="nav_item"><a href="/Pracownicy/agregat.php">Funkcje Agregujące</a></li>
-        <li class="nav_item"><a href="/Pracownicy/pracownicy.php">pracownicy</a></li>
-        <li class="nav_item"><a href="/Pracownicy/Data_i_Czas.php">Data i czas</a></li>
-        <li class="nav_item"><a href="/Dane-do-bazy/daneDoBazy.php">Dane do Bazy</a></li>
-        <li class="nav_item"><a href="/Biblioteka/ksiazki.php">Książki</a></li>
-        <li class="nav_item"><a href="/flexbox/flexbox.html">Flexbox</a></li>
-        <li class="nav_item"><a href="/index.php">strona główna</a></li>
-        <li class="nav_item"><a href="/Relacje/autortytul.php">Autor - Tytuł</a></li>
-        <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
+        <li><a class="navbar" href="/Pracownicy/orgPracownicy.php">Organizacja i Pracownicy</a></li>
+        <li><a class="navbar" href="/Pracownicy/agregat.php">Funkcje Agregujące</a></li>
+        <li><a class="navbar" href="/Pracownicy/pracownicy.php">pracownicy</a></li>
+        <li><a class="navbar" href="/Pracownicy/Data_i_Czas.php">Data i czas</a></li>
+        <li><a class="navbar" href="/Dane-do-bazy/daneDoBazy.php">Dane do Bazy</a></li>
+        <li><a class="navbar" href="/Biblioteka/ksiazki.php">Książki</a></li>
+        <li><a class="navbar" href="/flexbox/flexbox.html">Flexbox</a></li>
+        <li><a class="navbar" href="/index.php">strona główna</a></li>
+        <li><a class="navbar" href="/Relacje/autortytul.php">Autor - Tytuł</a></li>
+        <li><a class="navbar" href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
 </ul>
 
 NAV;
