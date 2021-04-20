53efb1059c41133b9576e541ef14ade5316f9ff3
Radek <r.kruczek03@gmail.com>
2021-04-20 18:31:29 +0200

    s

--- strona.php
+++ strona.php
@@ -32,6 +32,7 @@ $strona_php = <<<PHP
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name=description>
         <title>Radosław Kruczek</title>
         <link rel="stylesheet" href="style.css" />
     </head>
