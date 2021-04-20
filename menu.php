b98f947cc5bc3f067847ebf3505a281518f3f19c
Radek <r.kruczek03@gmail.com>
2021-04-20 09:23:13 +0200

    navbar

---
+++ menu.php
@@ -0,0 +1,12 @@
+<ul class="nav">
+        <li class="nav_item"><a href="Pracownicy/orgPracownicy.php">Organizacja i Pracownicy</a></li>
+        <li class="nav_item"><a href="Pracownicy/agregat.php">Funkcje Agregujące</a></li>
+        <li class="nav_item"><a href="Pracownicy/pracownicy.php">pracownicy</a></li>
+        <li class="nav_item"><a href="Pracownicy/Data_i_Czas.php">Data i czas</a></li>
+        <li class="nav_item"><a href="Dane-do-bazy/daneDoBazy.php">Dane do Bazy</a></li>
+        <li class="nav_item"><a href="Biblioteka/ksiazki.php">Książki</a></li>
+        <li class="nav_item"><a href="flexbox/flexbox.html">Flexbox</a></li>
+        <li class="nav_item"><a href="../index.php">strona główna</a></li>
+        <li class="nav_item"><a href="relacje.php">Relacje wiele do wielu</a></li>
+        <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
+        </ul>
