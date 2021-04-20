e1cf9898e93dd3de97686ba47e46b76c1983cfa5
Radek <r.kruczek03@gmail.com>
2021-04-20 11:23:58 +0200

    cl

---
+++ Relacje/nauczycielklasa.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Radosław Kruczek</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <?php include("../header.php"); ?>
+      </div>
+      <div class="menu">
+      <?php include("../menu.php"); ?>
+      </div>
+      <div class="main"></div>
+    </div>
+  </body>
+</html>
--- menu.php
+++ menu.php
@@ -7,6 +7,6 @@
         <li class="nav_item"><a href="/Biblioteka/ksiazki.php">Książki</a></li>
         <li class="nav_item"><a href="/flexbox/flexbox.html">Flexbox</a></li>
         <li class="nav_item"><a href="/index.php">strona główna</a></li>
-        <li class="nav_item"><a href="/Biblioteka/relacje.php">Relacje wiele do wielu</a></li>
+        <li class="nav_item"><a href="/Relacje/autortytul.php">Autor - Tytuł</a></li>
         <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
 </ul>
