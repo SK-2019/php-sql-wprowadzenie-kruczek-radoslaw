6b47b1c41941d9f6b9f1273a37bae6cebe7847f5
Radek <r.kruczek03@gmail.com>
2021-04-20 17:41:41 +0200

    test

--- strona.php
+++ strona.php
@@ -1,6 +1,31 @@
 <?php
 
-$strona_php = <<<STRONA
+$navbar = <<<NAV
+
+<ul class="nav">
+        <li class="nav_item"><a href="/Pracownicy/orgPracownicy.php">Organizacja i Pracownicy</a></li>
+        <li class="nav_item"><a href="/Pracownicy/agregat.php">Funkcje Agregujące</a></li>
+        <li class="nav_item"><a href="/Pracownicy/pracownicy.php">pracownicy</a></li>
+        <li class="nav_item"><a href="/Pracownicy/Data_i_Czas.php">Data i czas</a></li>
+        <li class="nav_item"><a href="/Dane-do-bazy/daneDoBazy.php">Dane do Bazy</a></li>
+        <li class="nav_item"><a href="/Biblioteka/ksiazki.php">Książki</a></li>
+        <li class="nav_item"><a href="/flexbox/flexbox.html">Flexbox</a></li>
+        <li class="nav_item"><a href="/index.php">strona główna</a></li>
+        <li class="nav_item"><a href="/Relacje/autortytul.php">Autor - Tytuł</a></li>
+        <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kruczek-radoslaw">Github</a></li>
+</ul>
+
+NAV;
+
+$header = <<< HEADER
+
+<h1 class="title">Radosław Kruczek</h1>
+<a class="click" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion SO</a>
+
+HEADER;
+
+
+$strona_php = <<<PHP
 
     <!DOCTYPE html>
     <html lang="en">
@@ -12,16 +37,19 @@ $strona_php = <<<STRONA
     <body>
         <div class="container">
         <div class="header">
-            <?php include("header.php"); ?>
+        {$header}
         </div>
         <div class="menu">
-        <?php include("menu.php"); ?>
+        {$navbar}
         </div>
         <div class="main"></div>
         </div>
     </body>
     </html>
 
-STRONA;
+PHP;
+
+
+
 
 ?>
