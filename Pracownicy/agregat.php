6e2c8556fcef05a46a49f30032f81f73289cd81d
Radek <r.kruczek03@gmail.com>
2021-04-23 09:31:48 +0200

    style

--- Pracownicy/agregat.php
+++ Pracownicy/agregat.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="/assets/style.css">
         <title>Pracownicy</title>
     </head>
 </html>
