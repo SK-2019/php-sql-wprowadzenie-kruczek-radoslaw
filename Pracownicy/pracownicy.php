95727b12165bd51723da0f8ac0ace4347f164b53
Radek <r.kruczek03@gmail.com>
2021-04-23 08:54:23 +0200

    prac

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -29,37 +29,13 @@
     echo("<h2>Zadanie 2</h2>");
     echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
     echo("<li>".$sql);
-    $result = $conn->query($sql);
-    echo("<table border=0>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>nazwa_dzial</th>");
-    echo("<th>zarobki</th>");
-    echo("<th>data_urodzenia</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+    table($sql);
 
     $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and zarobki < 30';
     echo("<h2>Zadanie 3</h2>");
     echo("<h3>Pracownicy tylko z zarobkami mniejszymi niż 30</h3>");
     echo("<li>".$sql);
-    $result = $conn->query($sql);
-    echo("<table border=0>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>nazwa_dzial</th>");
-    echo("<th>zarobki</th>");
-    echo("<th>data_urodzenia</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+    table($sql);
 ?>
     </div>
 </div>
