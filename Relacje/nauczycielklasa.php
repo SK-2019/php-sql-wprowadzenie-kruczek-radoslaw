6bdf7327eb6194998b86f00cc4b110f6e3d8cf96
Radek <r.kruczek03@gmail.com>
2021-04-25 14:50:28 +0200

    relacje

--- Relacje/nauczycielklasa.php
+++ Relacje/nauczycielklasa.php
@@ -34,7 +34,7 @@
           echo("</table>");
 
           $sql = "SELECT * FROM klasy";
-          echo("<h3>Nauczyciele</h3>");
+          echo("<h3>Klasy</h3>");
           echo("<li>".$sql);
           $result = $conn->query($sql) or die($conn->error);
           echo("<table border=0>");
@@ -44,6 +44,21 @@
           while($wiersz=$result->fetch_assoc()){
               echo("<tr>");
               echo("<td>".$wiersz['id_klasy']."</td><td>".$wiersz['klasa']."</td>");
+              echo("</tr>");
+          }
+          echo("</table>");
+
+          $sql = "SELECT * FROM nauczyciele, klasy, naucz_klasa where nauczyciel = id_Nauczyciela and klasa = id_klasy ";
+          echo("<h3>Nauczyciele i Klasy</h3>");
+          echo("<li>".$sql);
+          $result = $conn->query($sql) or die($conn->error);
+          echo("<table border=0>");
+          echo("<th>id_klasy</th>");
+          echo("<th>klasa</th>");
+
+          while($wiersz=$result->fetch_assoc()){
+              echo("<tr>");
+              echo("<td>".$wiersz['id']."</td><td>".$wiersz['nauczyciel']."</td><td>".$wiersz['klasa']."</td>");
               echo("</tr>");
           }
           echo("</table>");
