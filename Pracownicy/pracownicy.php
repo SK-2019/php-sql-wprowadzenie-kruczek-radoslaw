72427f40b2b04873fd5970507c29e275f22cfc34
Radek <r.kruczek03@gmail.com>
2021-04-25 14:08:11 +0200

    t

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -21,21 +21,19 @@
             echo("<h3>Pracownicy tylko z działu 2</h3>");
             echo("<li>".$p);
             table($p);
-
-            wow();
-            wow();
             
             $b = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
             echo("<h2>Zadanie 2</h2>");
             echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
             echo("<li>".$b);
             table2($b);
-
+/*
             $p = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and zarobki < 30';
             echo("<h2>Zadanie 3</h2>");
             echo("<h3>Pracownicy tylko z zarobkami mniejszymi niż 30</h3>");
             echo("<li>".$p);
             table($p);
+            */
         ?>
 
     </div>
