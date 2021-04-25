0a5d42ca73f2e963000dd67b06925514de183407
Radek <r.kruczek03@gmail.com>
2021-04-25 14:19:17 +0200

    t

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -15,14 +15,26 @@
     <div class="main">
         
         <?php
-            include_once("../assets/functions.php");
-            $p = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
+            require_once("../assets/connect.php");
+            $sql = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
             echo("<h2>Zadanie 1</h2>");
             echo("<h3>Pracownicy tylko z działu 2</h3>");
-            echo("<li>".$p);
-            table($p);
+            echo("<li>".$sql);
+            $result = $conn->query($sql);
+            echo("<table border=0>");
+            echo("<th>id_pracownicy</th>");
+            echo("<th>imie</th>");
+            echo("<th>nazwa_dzial</th>");
+            echo("<th>zarobki</th>");
+            echo("<th>data_urodzenia</th>");
+            while($wiersz=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
             
-            require_once("../assets/connect.php");
+            
             $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
             echo("<h2>Zadanie 2</h2>");
             echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
