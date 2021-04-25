a004e9ccfb758be1fe4f8e2832dd6b9b1ff26a8c
Radek <r.kruczek03@gmail.com>
2021-04-25 14:16:04 +0200

    t

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -22,18 +22,42 @@
             echo("<li>".$p);
             table($p);
             
-            $b = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
+            $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
             echo("<h2>Zadanie 2</h2>");
             echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
-            echo("<li>".$b);
-            table();
-/*
-            $p = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and zarobki < 30';
+            echo("<li>".$sql);
+            $result = $conn->query($sql);
+            echo("<table border=0>");
+            echo("<th>id_pracownicy</th>");
+            echo("<th>imie</th>");
+            echo("<th>nazwa_dzial</th>");
+            echo("<th>zarobki</th>");
+            echo("<th>data_urodzenia</th>");
+            while($wiersz=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
+
+            $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and zarobki < 30';
             echo("<h2>Zadanie 3</h2>");
             echo("<h3>Pracownicy tylko z zarobkami mniejszymi niż 30</h3>");
             echo("<li>".$p);
-            table($p);
-            */
+            $result = $conn->query($sql);
+            echo("<table border=0>");
+            echo("<th>id_pracownicy</th>");
+            echo("<th>imie</th>");
+            echo("<th>nazwa_dzial</th>");
+            echo("<th>zarobki</th>");
+            echo("<th>data_urodzenia</th>");
+            while($wiersz=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");   
+            
         ?>
 
     </div>
