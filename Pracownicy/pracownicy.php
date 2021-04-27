c654f47f82256dd98d1fd4caa3ea75a188795ad4
Radek <r.kruczek03@gmail.com>
2021-04-27 09:12:12 +0200

    fun

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -18,25 +18,26 @@
     <div class="main">
         
         <?php
+            include("../assets/functions.php");
             require_once("../assets/connect.php");
             $sql = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
             echo("<h2>Zadanie 1</h2>");
             echo("<h3>Pracownicy tylko z działu 2</h3>");
             echo("<li>".$sql);
-            $result = $conn->query($sql);
-            echo("<table border=0>");
-            echo("<th>id_pracownicy</th>");
-            echo("<th>imie</th>");
-            echo("<th>nazwa_dzial</th>");
-            echo("<th>zarobki</th>");
-            echo("<th>data_urodzenia</th>");
-            while($wiersz=$result->fetch_assoc()){
-                echo("<tr>");
-                echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-            echo("</table>");
-            
+            // $result = $conn->query($sql);
+            // echo("<table border=0>");
+            // echo("<th>id_pracownicy</th>");
+            // echo("<th>imie</th>");
+            // echo("<th>nazwa_dzial</th>");
+            // echo("<th>zarobki</th>");
+            // echo("<th>data_urodzenia</th>");
+            // while($wiersz=$result->fetch_assoc()){
+            //     echo("<tr>");
+            //     echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
+            //     echo("</tr>");
+            // }
+            // echo("</table>");
+            table($sql);
             
             $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
             echo("<h2>Zadanie 2</h2>");
