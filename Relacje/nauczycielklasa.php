916de21ede4ffae6886b9fab5d6ebbae7563b580
Radek <r.kruczek03@gmail.com>
2021-04-27 13:59:22 +0200

    fun

--- Relacje/nauczycielklasa.php
+++ Relacje/nauczycielklasa.php
@@ -17,52 +17,41 @@
       </div>
       <div class="main">
         <?php
+          ini_set('display_errors', '1');
+          ini_set('display_startup_errors', '1');
+          error_reporting(E_ALL);
+      
           require_once("../assets/connect.php");
+      
+      
+          function table($sql, $conn, $columnid, $column2, $dana, $dana2){
+      
+            $result = $conn->query($sql);
+            echo("<table border=0>");
+            echo("<th>$columnid</th>");
+            echo("<th>$column2</th>");
+            while($wiersz=$result->fetch_assoc()){
+              echo("<tr>");
+              echo("<td>".$wiersz[$dana2]."</td><td>".$wiersz[$dana]."</td>");
+              echo("</tr>");
+            }
+            echo("</table>");
+          }
+          
           $sql = "SELECT * FROM nauczyciele";
           echo("<h3>Nauczyciele</h3>");
           echo("<li>".$sql);
-          $result = $conn->query($sql) or die($conn->error);
-          echo("<table border=0>");
-          echo("<th>id_nauczyciela</th>");
-          echo("<th>nazwisko</th>");
-
-          while($wiersz=$result->fetch_assoc()){
-              echo("<tr>");
-              echo("<td>".$wiersz['id_Nauczyciela']."</td><td>".$wiersz['nazwisko']."</td>");
-              echo("</tr>");
-          }
-          echo("</table>");
+          table($sql, $conn, "id_nauczyciela", "nazwisko", 'nazwisko', 'id_nauczyciela');
 
           $sql = "SELECT * FROM klasy";
           echo("<h3>Klasy</h3>");
           echo("<li>".$sql);
-          $result = $conn->query($sql) or die($conn->error);
-          echo("<table border=0>");
-          echo("<th>id_klasy</th>");
-          echo("<th>klasa</th>");
-
-          while($wiersz=$result->fetch_assoc()){
-              echo("<tr>");
-              echo("<td>".$wiersz['id_klasy']."</td><td>".$wiersz['klasa']."</td>");
-              echo("</tr>");
-          }
-          echo("</table>");
+          table($sql, $conn, "id_klasy", "klasa", 'klasa', 'id_klasy');
 
           $sql = "SELECT * FROM nauczyciele, klasy, naucz_klasa where nazwa_nauczyciel = id_Nauczyciela and nazwa_klasa = id_klasy ";
           echo("<h3>Nauczyciele i Klasy</h3>");
           echo("<li>".$sql);
-          $result = $conn->query($sql) or die($conn->error);
-          echo("<table border=0>");
-          echo("<th>id</th>");
-          echo("<th>nauczyciel</th>");
-          echo("<th>klasa</th>");
-
-          while($wiersz=$result->fetch_assoc()){
-              echo("<tr>");
-              echo("<td>".$wiersz['id']."</td><td>".$wiersz['nazwisko']."</td><td>".$wiersz['klasa']."</td>");
-              echo("</tr>");
-          }
-          echo("</table>");
+          table($sql, $conn, "nauczyciel", "klasa", 'nazwisko', 'klasa');
         ?>
       </div>
     </div>
