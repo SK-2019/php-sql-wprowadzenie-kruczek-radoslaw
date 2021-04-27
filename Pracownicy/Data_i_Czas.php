9e3bbdc3b5443c47e601cffd6c7c6686a507a499
Radek <r.kruczek03@gmail.com>
2021-04-27 10:43:32 +0200

    function

--- Pracownicy/Data_i_Czas.php
+++ Pracownicy/Data_i_Czas.php
@@ -41,6 +41,32 @@ function table($sql, $conn){
     echo("</table>");
 }
 
+function table2($sql, $conn){
+
+    $result = $conn->query($sql);
+    echo("<table border=0>");
+    echo("<th>suma lat</th>");
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['suma_wiek']."</td>");
+        echo("</tr>");
+    }
+echo("</table>");
+}
+
+function table3($sql, $conn){
+
+    $result = $conn->query($sql);
+    echo("<table border=0>");
+    echo("<th>średnia</th>");
+    echo("<th>dzial</th>");
+        while($wiersz=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>".$wiersz['srednia_wiek']."</td><td>".$wiersz['nazwa_dzial']."</td>");
+            echo("</tr>");
+        }
+    echo("</table>");
+}
 
 require_once("../assets/connect.php");
 $sql = 'SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy, organizacja where dzial=id_org;';
@@ -59,87 +85,37 @@ $sql = 'SELECT * ,sum(YEAR(curdate())-YEAR(data_urodzenia)) AS suma_wiek FROM pr
 echo("<h2>Zadanie 3</h2>");
 echo("<h3>suma lat wszystkich pracowników</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>suma lat</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['suma_wiek']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table2($sql, $conn);
 
 $sql = 'SELECT * ,sum(YEAR(curdate())-YEAR(data_urodzenia)) AS suma_wiek FROM pracownicy, organizacja where dzial = id_org and dzial = 2';
 echo("<h2>Zadanie 4</h2>");
 echo("<h3>suma lat pracowników z działu handel</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>suma lat z działu handel</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['suma_wiek']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table2($sql, $conn);
 
 $sql = 'SELECT * ,sum(YEAR(curdate())-YEAR(data_urodzenia)) AS suma_wiek FROM pracownicy, organizacja where dzial = id_org and dzial = 2 and imie like "%a"';
 echo("<h2>Zadanie 5</h2>");
 echo("<h3>suma lat kobiet z działu handel</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>suma lat z działu handel</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['suma_wiek']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table2($sql, $conn);
 
 $sql = 'SELECT * ,sum(YEAR(curdate())-YEAR(data_urodzenia)) AS suma_wiek FROM pracownicy, organizacja where dzial = id_org and dzial = 2 and imie not like "%a"';
 echo("<h2>Zadanie 6</h2>");
 echo("<h3>suma lat mężczyzn z działu handel</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>suma lat z działu handel</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['suma_wiek']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table2($sql, $conn);
 
 $sql = 'SELECT * ,avg(YEAR(curdate())-YEAR(data_urodzenia)) AS srednia_wiek FROM pracownicy, organizacja where dzial = id_org group by dzial';
 echo("<h2>Zadanie 7</h2>");
 echo("<h3>średnia lat pracowników w poszczególnych działach</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>średnia lat z poszczególnych działów</th>");
-echo("<th>dzial</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['srednia_wiek']."</td><td>".$wiersz['nazwa_dzial']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table3($sql, $conn);
 
 $sql = 'SELECT * ,sum(YEAR(curdate())-YEAR(data_urodzenia)) AS suma_wiek FROM pracownicy, organizacja where dzial = id_org group by dzial';
 echo("<h2>Zadanie 8</h2>");
 echo("<h3>suma lat pracowników w poszczególnych działach</h3>");
 echo("<li>".$sql);
-$result = $conn->query($sql);
-echo("<table border=0>");
-echo("<th>suma lat z poszczególnych działów</th>");
-echo("<th>dzial</th>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['suma_wiek']."</td><td>".$wiersz['nazwa_dzial']."</td>");
-        echo("</tr>");
-    }
-echo("</table>");
+table2($sql, $conn);
 
 $sql = 'SELECT *, max(YEAR(curdate())-YEAR(data_urodzenia)) AS najstarszy FROM pracownicy, organizacja where dzial = id_org group by dzial';
 echo("<h2>Zadanie 9</h2>");
