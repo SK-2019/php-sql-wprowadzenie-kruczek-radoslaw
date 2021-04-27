b96ace6f9ce0c072dc5fddbf761a7d70c2d385d4
Radek <r.kruczek03@gmail.com>
2021-04-27 10:12:20 +0200

    conn

--- assets/connect.php
+++ assets/connect.php
@@ -1,6 +1,7 @@
 <?php
 //require_once("config.php");
-global $conn = new mysqli($_SERVER['MYSQL_SERV'],$_SERVER['MYSQL_NAME'], $_SERVER['MYSQL_PS'], $_SERVER['MYSQL_DB']);
+global $conn;
+$conn = new mysqli($_SERVER['MYSQL_SERV'],$_SERVER['MYSQL_NAME'], $_SERVER['MYSQL_PS'], $_SERVER['MYSQL_DB']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
     }
