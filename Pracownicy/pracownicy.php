d705eb28b530b439bf535f5c631db89e262feb82
Radek <r.kruczek03@gmail.com>
2021-04-27 10:16:02 +0200

    function

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -42,32 +42,17 @@
                 echo("</table>");
             }
 
-            //include("../assets/functions.php");
-            //require_once("../assets/connect.php");
-            //$p = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
+            $sql = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
             echo("<h2>Zadanie 1</h2>");
             echo("<h3>Pracownicy tylko z działu 2</h3>");
-            //echo("<li>".$p);
-            // $result = $conn->query($sql);
-            // echo("<table border=0>");
-            // echo("<th>id_pracownicy</th>");
-            // echo("<th>imie</th>");
-            // echo("<th>nazwa_dzial</th>");
-            // echo("<th>zarobki</th>");
-            // echo("<th>data_urodzenia</th>");
-            // while($wiersz=$result->fetch_assoc()){
-            //     echo("<tr>");
-            //     echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
-            //     echo("</tr>");
-            // }
-            // echo("</table>");
-            table("SELECT * FROM pracownicy, organizacja where dzial = id_org", $conn);
+            echo("<li>".$sql);
+            table($sql, $conn);
             
-            //$sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
+            $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)';
             echo("<h2>Zadanie 2</h2>");
             echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
-            //echo("<li>".$sql);
-            table('SELECT * FROM pracownicy, organizacja where dzial = id_org and dzial in(2, 3)');
+            echo("<li>".$sql);
+            table($sql, $conn);
 /*
             $sql = 'SELECT * FROM pracownicy, organizacja where dzial = id_org and zarobki < 30';
             echo("<h2>Zadanie 3</h2>");
