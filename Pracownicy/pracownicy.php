c8a1ee0ac7f7058812a42c451df1cf25442a9ab0
Radek <r.kruczek03@gmail.com>
2021-04-27 09:19:54 +0200

    functions

--- Pracownicy/pracownicy.php
+++ Pracownicy/pracownicy.php
@@ -18,7 +18,7 @@
     <div class="main">
         
         <?php
-            include("../assets/functions.php");
+            //include("../assets/functions.php");
             //require_once("../assets/connect.php");
             $p = "SELECT * FROM pracownicy, organizacja where dzial = id_org";
             echo("<h2>Zadanie 1</h2>");
@@ -75,6 +75,26 @@
             }
             echo("</table>");   
             
+
+
+
+            function table($sql){
+                require_once("connect.php");
+                $result = $conn->query($sql);
+                echo("<table border=0>");
+                echo("<th>id_pracownicy</th>");
+                echo("<th>imie</th>");
+                echo("<th>nazwa_dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>data_urodzenia</th>");
+                while($wiersz=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$wiersz['id_pracownicy']."</td><td>".$wiersz['imie']."</td><td>".$wiersz['nazwa_dzial']."</td><td>".$wiersz['zarobki']."</td><td>".$wiersz['data_urodzenia']."</td>");
+                    echo("</tr>");
+                }
+                echo("</table>");
+                $conn->close();
+            }
         ?>
 
     </div>
