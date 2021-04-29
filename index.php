1e95959b50a342a36bf49837e0a94bd6a56ee355
Radek <r.kruczek03@gmail.com>
2021-04-29 22:47:12 +0200

    index

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?php
-
-include("assets/strona.php");
-
-echo $strona_php;
-
-?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Radosław Kruczek</title>
+        <link rel="stylesheet" href="assets/style.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+            <?php include("assets/header.php"); ?>
+            </div>
+            <div class="menu">
+            <?php include("assets/menu.php");   ?>
+            </div>
+            <div class="main"></div>
+        </div>
+    </body>
+    </html>
