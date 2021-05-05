dad6a9f1cbf590f10efd9a7ac5b8d17d8015f54e
Radek <r.kruczek03@gmail.com>
2021-05-05 21:47:43 +0200

    del

--- Relacje/nauczycielklasa.php
+++ Relacje/nauczycielklasa.php
@@ -37,8 +37,8 @@
 		
               <form action='delete.php' method='POST'>
               <input type='number' name='row' value='".$wiersz[$dana2]."' hidden>
-              <input type='number' name='table' value=$table hidden>
-              <input type='number' name='column' value=$columnid hidden>
+              <input type='number' name='table' value=".$table." hidden>
+              <input type='number' name='column' value=".$columnid." hidden>
               <input type='submit' value='Usuń'>
          </form>
               
