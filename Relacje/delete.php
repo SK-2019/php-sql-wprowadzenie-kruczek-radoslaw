8fec2cf16a2959beafdbc02858fb91010d1ec6e5
Radek <r.kruczek03@gmail.com>
2021-05-06 19:33:17 +0200

    del

--- Relacje/delete.php
+++ Relacje/delete.php
@@ -1,32 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Radosław Kruczek</title>
+    <link rel="stylesheet" href="/assets/style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <?php include("../assets/header.php"); ?>
+      </div>
+      <div class="menu">
+      <?php include("../assets/menu.php"); ?>
+      </div>
+      <div class="main">
+
+        <?php
+                  ini_set('display_errors', '1');
+                  ini_set('display_startup_errors', '1');
+                  error_reporting(E_ALL);
+        echo("<h1>jestes w delete.php </h1>");
 
 
-<?php
-          ini_set('display_errors', '1');
-          ini_set('display_startup_errors', '1');
-          error_reporting(E_ALL);
-echo("<h1>jestes w delete.php </h1>");
 
+        require_once("../assets/connect.php");
 
+        function delete($conn, $table, $column, $row){
+        //definiujemy zapytanie $sql
+        $sql = "DELETE  FROM $table WHERE $column = $row";
 
-require_once("../assets/connect.php");
+        //wyświetlamy zapytanie $sql
+        echo $sql;
 
-function delete($conn, $table, $column, $row){
-//definiujemy zapytanie $sql
-$sql = "DELETE  FROM $table WHERE $column = $row";
+        if ($conn->query($sql) === TRUE) {
+          echo ("<h2>Record deleted successfully</h2>");
 
-//wyświetlamy zapytanie $sql
-echo $sql;
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
 
-if ($conn->query($sql) === TRUE) {
-  echo ("<h2>Record deleted successfully</h2>");
+        $conn->close();
+        }
 
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+        delete($conn, $_POST['table'], $_POST['column'], $_POST['row']);
 
-$conn->close();
-}
-
-delete($conn, $_POST['table'], $_POST['column'], $_POST['row']);
-
-?>
+        ?>
+      </div>
+    </div>
+  </body>
+</html>
