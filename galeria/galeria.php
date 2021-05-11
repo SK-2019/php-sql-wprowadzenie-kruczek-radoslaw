4d28a3efda2322f4ddca88e59cdbdf7da3e1e219
Radek <r.kruczek03@gmail.com>
2021-05-11 09:47:12 +0200

    galeria

---
+++ galeria/galeria.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Radosław Kruczek</title>
+        <link rel="stylesheet" href="assets/style.css" />
+    </head>
+    <body>
+    <div class="container">
+        <div class="header">
+        <?php include("assets/header.php"); ?>
+        </div>
+        <div class="menu">
+        <?php include("assets/menu.php");   ?>
+        </div>
+        <div class="main">
+            <div class="gallery row">
+                <div class="image foto01">1</div>
+                <div class="image foto02">2</div>
+                <div class="image foto03">3</div>
+            </div>
+            <div class="gallery row">
+            <a href="#" class="image foto01">
+                <span class="foto01_title">kurs jazdy moplikiem</span>
+                <span class="foto01_price">cena: 299 zł</span>
+            </a>
+            <a href="#" class="image foto02">
+                <span class="foto01_title">kurs jazdy moplikiem</span>
+                <span class="foto01_price">cena: 299 zł</span>
+            </a>
+            <a href="#" class="image foto03">
+                <span class="foto01_title">kurs nurkowania</span>
+                <span class="foto01_price">cena: 1299 zł</span></a>
+            </div>
+            <div class="gallery row">
+                <div class="image foto01">1</div>
+                <div class="image foto02">2</div>
+                <div class="image foto03">3</div>
+            </div>
+            
+        </div>
+    </div>
+    </body>
+    </html>
